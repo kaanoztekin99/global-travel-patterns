2f5cee9e595ab2e3dad8829406c85ab7e233f965
leonardoNNanci <leonardonanci@id.uff.br>
2026-05-05 16:52:34 +0200

    load arrivals data to map

--- code/js/map.js
+++ code/js/map.js
@@ -15,17 +15,43 @@ function initMap() {
     const response = await fetch("../data/geojson/countries.geojson");
     const geojson = await response.json();
 
+    // load data from country arrivals dataset
+    const countriesData = await fetch("../data/clean/countries.csv");
+    const countriesCsv = await countriesData.text();
+    const arrivals = d3.csvParse(countriesCsv);
+    const arrivalsMap = new Map(
+      arrivals.map((d) => [d.iso_code.toLowerCase(), d])
+    );
+    const arrivalsRange = d3.extent(arrivals, (d) => {
+      if (!d.iso_code) return null;
+      var value = 0;
+      for (let year = 2020; year >= 1995; year--) {
+        if (d[year.toString()] > value) {
+          value = +d[year.toString()];
+        }
+      }
+      return isNaN(value) ? null : value;
+    });
+    
     geojson.features.forEach((feature, i) => {
       const p = feature.properties || (feature.properties = {});
+      const arrivalsData = arrivalsMap.get(p.cca2);
 
       p.display_name =
         p.name ||
         p.NAME ||
         p.ADMIN ||
         p.shapeName ||
+        arrivalsData?.name ||
         `Country ${i + 1}`;
 
-      p.dummy_score = 40 + ((i * 7) % 61);
+      p.arrival_count = null;
+      for (let year = 2020; year >= 1995; year--) {
+        if (arrivalsData?.[year.toString()] > p.arrival_count) {
+          p.arrival_count = +arrivalsData[year.toString()];
+          p.year = year;
+        }
+      }
     });
 
     map.addSource("countries", {
@@ -41,10 +67,9 @@ function initMap() {
         "fill-color": [
           "interpolate",
           ["linear"],
-          ["get", "dummy_score"],
-          40, "#fde0ef",
-          70, "#c51b8a",
-          100, "#7a0177"
+          ["get", "arrival_count"],
+          arrivalsRange[0], "#fde0ef",
+          arrivalsRange[1], "#7a0177"
         ],
         "fill-opacity": 0.65
       }
@@ -73,7 +98,8 @@ function initMap() {
         .setLngLat(e.lngLat)
         .setHTML(`
           <strong>${props.display_name}</strong><br>
-          Score: ${props.dummy_score}
+          Arrivals: ${new Intl.NumberFormat("de-DE").format(props.arrival_count)}<br>
+          Year: ${props.year}
         `)
         .addTo(map);
     });
